ce502044a7a90c134ea413835b6322e13c5ac8cd
Ginanjar1933 <kiminoudin8@gmail.com>
2025-04-22 15:08:40 +0700

    Update google.js

--- server/config/google.js
+++ server/config/google.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const googleConfig = {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/api/auth/google/callback',
+    callbackURL: 'https://toko-ginanjar-production.up.railway.app/api/auth/google/callback',
     responseType: 'json',
     scope: ['email', 'profile']
 };
